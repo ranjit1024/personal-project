38802e9308d959d50860a8a3276979df4f46b4a5
ranjit1024 <ranjitdas2048@gmail.com>
2024-11-17 21:47:03 +0530

    adding resoponsive ness

--- javascript/index.js
+++ javascript/index.js
@@ -1,34 +1,69 @@
-const main = document.querySelector(".container");
-const contactButton = document.querySelector(".contact-us");
-const contact = document.querySelector(".contact");
 
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry =>{
-        if(entry.isIntersecting){
-            entry.target.classList.add('show')
-        }
+const contactButton = document.querySelector(".contact-us");
+const contactcontainer = document.querySelector(".contact-container");
+
+
+const main = document.querySelector(".container");
+const nav = document.querySelector("nav");
+const footer = document.querySelector("footer");
+
+const mainPage = document.querySelector(".main-page i");
+
+const mainPageButton = document.querySelector('.main-page')
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
     });
-},{
+  },
+  {
     // threshold: 0.2
-})
+  }
+);
 
 const hidelElement = document.querySelectorAll(".hide");
-hidelElement.forEach(el => observer.observe(el));
+hidelElement.forEach((el) => observer.observe(el));
 
-const observe2 = new IntersectionObserver(entries =>{
-   entries.forEach(entry =>{
-    if(entry.isIntersecting){
-        entry.target.classList.add("show2")
+const observe2 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show2");
     }
-   })
-})
+  });
+});
 
 const leftToRightElement = document.querySelectorAll(".hide2");
-leftToRightElement.forEach(el => observe2.observe(el))
+leftToRightElement.forEach((el) => observe2.observe(el));
 
-//adding constact us feture
-contactButton.addEventListener("click", e =>{
+
+//redirecting to main page
+contactButton.addEventListener('click', e =>{
+    nav.classList.add("main-hide");
     main.classList.add("main-hide");
-    contact.classList.remove("main-hide")
+    footer.classList.add("main-hide");
+    contactcontainer.classList.remove("main-hide")
+})
+
+//settin up back to mai page
+mainPageButton.addEventListener('click', e =>{
+    nav.classList.remove("main-hide");
+    main.classList.remove("main-hide");
+    footer.classList.remove("main-hide");
+    contactcontainer.classList.add("main-hide")
+})
+
+//stting up a hover effect
+mainPage.addEventListener('mouseover', e =>{
+    mainPage.classList.remove('bi-arrow-left-circle')
+    mainPage.classList.add('bi-arrow-left-circle-fill')
+    
+})
+
+mainPage.addEventListener('mouseout', e =>{
+    mainPage.classList.remove('bi-arrow-left-circle-fill')
+    mainPage.classList.add('bi-arrow-left-circle')
     
 })
